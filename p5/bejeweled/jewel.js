0aea03977970513afc9dd243e1789fe2ba03bb3c
Liwady <liwadyv@gmail.com>
2018-12-19 22:37:37 +0100

    idk man fuck life
    :pig:

--- p5/bejeweled/jewel.js
+++ p5/bejeweled/jewel.js
@@ -1,24 +1,24 @@
 var img;
 var canvas;
-var rij = 5;
+var row = 5;
 var col = 5;
+var board = [];
 
 function setup() {
 
   img = createImg("https://images7.memedroid.com/images/UPLOADED971/5958d10406a9c.jpeg");
   canvas = createCanvas(500, 500);
-
+  for (var i = 0; i < col; i ++) {
+    board[i]=[];
+    for (var j = 0; j < row; j++)
+      board[i][j]= random(255);
+  }
   img.position(0, 50);
   img.size(500, 500);
   canvas.position(500, 50);
 }
 
 function draw() {
-  background(0, 0, 0);
-  for (var i = 0; i < col; i ++) {
-    for (var j = 0; j < row; i++)
-      stroke(0);
-      fill(255);
-      rect(i*30,j*30,30,30);
-  }
+  background(50);
+
 }
