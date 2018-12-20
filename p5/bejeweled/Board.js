2b3c6e0d0b5adf547a049669bcb01e61fbd8eda3
Liwady <liwadyv@gmail.com>
2018-12-20 10:18:01 +0100

    ICE LIL BIT OF LIQ
    :ocean:

---
+++ p5/bejeweled/Board.js
@@ -0,0 +1,25 @@
+var img;
+var canvas;
+var row = 5;
+var col = 5;
+var board = [];
+
+function setup() {
+
+  img = createImg("https://images7.memedroid.com/images/UPLOADED971/5958d10406a9c.jpeg");
+  canvas = createCanvas(500, 500);
+
+  img.position(0, 50);
+  img.size(500, 500);
+  canvas.position(500, 50);
+}
+
+function draw() {
+  background(50);
+  for (var i = 0; i < col; i ++) {
+    board[i]=[];
+    for (var j = 0; j < row; j++)
+      
+  }
+
+}
