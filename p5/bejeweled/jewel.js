26fe57fb9ace7ddbec4bdaaf6f9c54886e95c20c
SanderVerbeken <sander.verbeken@student.ucll.be>
2018-12-20 11:26:11 +0100

    Update jewel.js
    dab0.2

--- p5/bejeweled/jewel.js
+++ p5/bejeweled/jewel.js
@@ -1,1 +1,29 @@
-dfghjghg
+var color;
+var shape;
+
+class Jewel{
+    setColor();
+    setShape();
+}
+function setShape(){
+    this.shape = "circle";
+}
+
+function setColor(){
+    
+   var number = Math.random(3);
+    switch(number){
+        case 0:
+            this.color = "Yellow";
+            break;
+        case 1:
+            this.color = "Red";
+            break;
+        case 2:
+            this.color = "Blue";
+            break;
+        case 3:
+            this.color = "Green"
+            break;
+    }
+}
