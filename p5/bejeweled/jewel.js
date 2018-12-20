0498cc87df6b2b92ecde63964d6e7a880c37abf6
SanderVerbeken <sander.verbeken@student.ucll.be>
2018-12-20 11:44:21 +0100

    YEET
    YAH Yeet

--- p5/bejeweled/jewel.js
+++ p5/bejeweled/jewel.js
@@ -1,29 +1,53 @@
 var color;
 var shape;
+var positionX;
+var positionY;
 
 class Jewel{
-    setColor();
-    setShape();
-}
+     Jewel(arrayJewel) {
+        setColor();
+        setShape();
+        setPositionX(arrayJewel);
+        setPositionY(arrayJewel);
+    }
+}    
 function setShape(){
     this.shape = "circle";
 }
-
+        
 function setColor(){
+            
+    var number = Math.random(3);
+        switch(number){
+            case 0:
+                this.color = "Yellow";
+                break;
+            case 1:
+                this.color = "Red";
+                break;
+            case 2:
+                this.color = "Blue";
+                break;
+            case 3:
+                this.color = "Green"
+                break;
+            }
+}
+function setPositionX(arrayJewel){
+    try {
+        arrayJewel[0] > 0 && arrayJewel[0] < arrayJewel.length - 1
+      }
+      catch(err) {
+        return null;
+      }
     
-   var number = Math.random(3);
-    switch(number){
-        case 0:
-            this.color = "Yellow";
-            break;
-        case 1:
-            this.color = "Red";
-            break;
-        case 2:
-            this.color = "Blue";
-            break;
-        case 3:
-            this.color = "Green"
-            break;
-    }
+}
+function setPositionY(arrayJewel){
+    try {
+        arrayJewel[0] > 0 && arrayJewel[0] < arrayJewel.length - 1
+      }
+      catch(err) {
+        return null;
+      }
+              
 }
